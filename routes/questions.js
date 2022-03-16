169dcc9731f15fe6f164ff3cebebb4487fb25cdf
JayDrojas <damianxrojas@gmail.com>
2022-03-16 13:36:40 -0700

    user error when not logged in and accessing a questions detail, this is a temporary fix to requireAuth

--- routes/questions.js
+++ routes/questions.js
@@ -4,6 +4,7 @@ const bcrypt = require('bcryptjs');
 const db = require('../db/models');
 const { csrfProtection, asyncHandler} = require('./utils');
 const { validationResult, check } = require('express-validator');
+const { requireAuth } = require('../auth');
 
 const router = express.Router();
 
@@ -15,7 +16,7 @@ router.get('/', asyncHandler(async (req, res) => {
     res.render('questions.pug', { title: 'Questions', allQuestions})
 }));
 
-router.get('/:id(\\d+)', asyncHandler(async (req, res) => {
+router.get('/:id(\\d+)', requireAuth, asyncHandler(async (req, res) => {
     const questionId = parseInt(req.params.id, 10);
     const question = await db.Question.findByPk(questionId, {
         include: { model: db.Answer, include: { model: db.Comment } }
@@ -23,8 +24,11 @@ router.get('/:id(\\d+)', asyncHandler(async (req, res) => {
     let activeUser;
     if (req.session.auth.userId){
         activeUser = req.session.auth.userId;
+    } else {
+        activeUser = null;
     }
-    console.log(req.session.auth.userId)
+    console.log('----------------------> \n\n', req.session.auth.userId)
+    console.log('----------------------> \n\n', question.userId)
     res.render('question-detail', { question, title: 'Details', activeUser });
 }));
 
