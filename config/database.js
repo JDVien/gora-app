17394b91f9a67a561abc7cf781476e58ccb19df9
jasonv <jdvn2517@gmail.com>
2022-03-16 14:45:52 -0700

    config db file ssl=true

--- config/database.js
+++ config/database.js
@@ -14,9 +14,6 @@ module.exports = {
   production: {
     use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
-    "dialectOptions": {
-      "ssl": true
-    },
     seederStorage: 'sequelize',
   }
 };
