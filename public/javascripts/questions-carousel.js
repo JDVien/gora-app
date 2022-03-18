dc4b8f8adafe3b078910503a1ab3638c31b7a76e
JayDrojas <damianxrojas@gmail.com>
2022-03-18 15:29:45 -0700

    updated styling to use a carousel

--- public/javascripts/questions-carousel.js
+++ public/javascripts/questions-carousel.js
@@ -1,26 +1,89 @@
 
-let i = 0;
-const getQuestion = async () => {
-  const nextQuestionBttn = document.querySelector('.get-question')
-  const carousel = document.querySelector('.question-divs')
+// let i = 0;
+// const getQuestion = async () => {
+//   const nextQuestionBttn = document.querySelector('.get-question')
+//   const carousel = document.querySelector('.question-divs')
+//   const questionResponse = await fetch('/questions/all');
+//   const questionData = await questionResponse.json();
+//   const questionDiv = document.querySelector('.questions-divs')
+//   console.log(questionData)
+// nextQuestionBttn.onclick = function() {
+
+//     console.log(i)
+
+//     questionDiv.innerHTML = `
+//         <div class='carousel-item'>
+//           <p>${questionData[i].title}</p>
+//           <a href=/questions/${questionData[i].id}> <img src=${questionData[i].imgLink}></a>
+//         </div>
+//     `
+//     i++;
+//   }
+// }
+// getQuestion();
+// window.addEventListener('DOMContentLoaded', async (event) => {
+
   const questionResponse = await fetch('/questions/all');
   const questionData = await questionResponse.json();
-  const questionDiv = document.querySelector('.questions-divs')
+  const questionDiv = document.querySelector('.carousel')
   console.log(questionData)
-nextQuestionBttn.onclick = function() {
 
-    console.log(i)
-
-    questionDiv.innerHTML = `
-      <div class='carousel'>
-        <div class='carousel-item'>
-          <p>${questionData[i].title}</p>
-          <a href=/questions/${questionData[i].id}> <img src=${questionData[i].imgLink}></a>
-        </div>
-      </div>
-
+  // <a href=/questions/${question.id}>
+  questionData.forEach(question => {
+    questionDiv.innerHTML += `
+    <div class='carousel_item carousel_item--hidden'>
+    <p>${question.title}</p>
+    <a href=/questions/${question.id}><img src=${question.imgLink}></a>
+    </div>
     `
-    i++;
+  })
+
+
+  let slidePosition = 0;
+
+  const slides = document.getElementsByClassName('carousel_item')
+  // console.log(slides[slidePosition])
+
+  slides[slidePosition].classList.remove('carousel_item--hidden')
+  slides[slidePosition].classList.add('carousel_item--visible')
+  const totalSlides = slides.length;
+
+  document.
+  getElementById('carousel_button--next')
+  .addEventListener("click", (e) => {
+    moveToNextSlide();
+  });
+
+  document.
+  getElementById('carousel_button--prev')
+  .addEventListener("click", function () {
+    moveToPrevSlide();
+  });
+
+  function updateSlidePosition() {
+    for (let slide of slides) {
+      slide.classList.remove('carousel_item--visible');
+      slide.classList.add('carousel_item--hidden');
+    }
+    slides[slidePosition].classList.add('carousel_item--visible');
   }
-}
-getQuestion();
+
+  function moveToNextSlide() {
+    if (slidePosition === totalSlides - 1) {
+      slidePosition = 0;
+    } else {
+      slidePosition++;
+    }
+    updateSlidePosition();
+  }
+
+  function moveToPrevSlide() {
+    if (slidePosition === 0) {
+      slidePosition = totalSlides - 1;
+    } else {
+      slidePosition--;
+    }
+    updateSlidePosition();
+  }
+
+// })
