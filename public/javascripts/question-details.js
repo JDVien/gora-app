f3d277fa4a598e8ea8f90626e3294b9a57ebfe98
JayDrojas <damianxrojas@gmail.com>
2022-03-19 16:31:20 -0700

    fixed error another error in question-details.js

--- public/javascripts/question-details.js
+++ public/javascripts/question-details.js
@@ -13,25 +13,31 @@ const editBtn = document.querySelector(".edit-button");
 
         const deleteQueBtn = document.querySelector(".delete-question");
 
-        deleteQueBtn.addEventListener('click', (e) => {
-            const questionId = e.target.id.split('-')[2];
-            const form = document.getElementById(`delete-question-${questionId}`)
-            if (form.classList.contains('hidden')) {
-                form.classList.remove('hidden')
-            } else {
-                form.classList.add('hidden')
-            }
-        })
+        if(deleteQueBtn) {
+
+            deleteQueBtn.addEventListener('click', (e) => {
+                const questionId = e.target.id.split('-')[2];
+                const form = document.getElementById(`delete-question-${questionId}`)
+                if (form.classList.contains('hidden')) {
+                    form.classList.remove('hidden')
+                } else {
+                    form.classList.add('hidden')
+                }
+            })
+        }
         const deleteAnsBtn = document.querySelector(".delete-answer");
 
-        deleteAnsBtn.addEventListener('click', (e) => {
-            const answerId = e.target.id.split('-')[2];
-            const form = document.getElementById(`delete-answer-${answerId}`)
-            if (form.classList.contains('hidden')) {
-                form.classList.remove('hidden')
-            } else {
-                form.classList.add('hidden')
-            }
-        })
+        if(deleteAnsBtn) {
 
+            deleteAnsBtn.addEventListener('click', (e) => {
+                const answerId = e.target.id.split('-')[2];
+                const form = document.getElementById(`delete-answer-${answerId}`)
+                if (form.classList.contains('hidden')) {
+                    form.classList.remove('hidden')
+                } else {
+                    form.classList.add('hidden')
+                }
+            })
+
+        }
     }
