cb68ae3f9f0ca37152a7d74cbaa568aaa3ffc399
Nathan Zavala <zavalanathana@gmail.com>
2022-03-20 13:39:31 -0700

    Added comments seed data

--- db/seeders/20220314232709-comments-data.js
+++ db/seeders/20220314232709-comments-data.js
@@ -10,34 +10,68 @@ module.exports = {
       */
    return queryInterface.bulkInsert('Comments', [
     {
-      content: "",
-      userId: 2,
+      content: "That's what I was thinking, not sure though",
+      userId: 1,
       answerId: 1,
       createdAt: new Date(),
       updatedAt: new Date()
     },
     {
-      userId: 2,
-      answerId: 2,
-      content: "100% agreed! RE4 was definitely very hard.",
-      createdAt: new Date(),
-      updatedAt: new Date()
-    },
-    {
-      content: "Pretty sure someone survived at the end of this movie.",
-      userId: 4,
-      answerId: 1,
-      createdAt: new Date(),
-      updatedAt: new Date()
-    },
-    {
-      content: "first!",
+      content: "That's right, that is def what happens",
       userId: 2,
       answerId: 3,
       createdAt: new Date(),
       updatedAt: new Date()
-    }
-
+    },
+    {
+      content: "Ok, yeah, in the original ending everyone died, but then Paramount changed it for theaters. Now I remember what I was thinking",
+      userId: 4,
+      answerId: 3,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      content: "100% agreed! RE4 was definitely very hard.",
+      userId: 2,
+      answerId: 4,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      content: "hard yes, but such a great game",
+      userId: 4,
+      answerId: 4,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      content: "yeah, for sure!",
+      userId: 2,
+      answerId: 4,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      content: "I feel like this would make the most sense",
+      userId: 4,
+      answerId: 8,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      content: "definitely one of the main points the author was going for",
+      userId: 1,
+      answerId: 40,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      content: "yeah there isn't anyone in this story who is completely innocent",
+      userId: 3,
+      answerId: 40,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
    ], {});
   },
   down: (queryInterface, Sequelize) => {
